a555b060cea9906c5086ea7aa1541f18c479d2e8
yushan <yushanwebdev@gmail.com>
2022-04-03 16:47:48 +0530

    feat: Scaffolding a Multi-Panel App Layout with Flexbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
 const App = () => {
   return (
-    <div className="bg-gray-700 text-white min-h-screen flex items-center justify-center">
-      <div className="max-w-lg">
-        <div className="flex hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
-          <img
-            className="w-10 h-10 rounded-full mr-4"
-            src="https://i.pravatar.cc/300"
-          />
-          <div>
-            <p className="flex items-baseline">
-              <span className="text-emerald-600 text-sm mr-2 font-medium">
-                adamawathan
-              </span>
-              <span className="text-gray-400 text-xs">01/15/2021</span>
-            </p>
-            <p className="text-slate-300">
-              I write slowly when I'm not super confident with a language, and
-              when I tried to do JS/TS last year for speed, I consistently
-              messed up. like it was an interview or something.
-            </p>
-          </div>
+    <div className="flex text-white h-screen">
+      <div className="bg-gray-800 p-4">
+        <div className="bg-white text-gray-800 w-12 h-12 flex items-center justify-center rounded-full">
+          TW
         </div>
+      </div>
+      <div className="bg-gray-700 w-60 flex flex-col">
+        <div className="p-4 shadow-md">Tailwind CSS</div>
+        <div className="p-4 flex-1">Channels</div>
+      </div>
+      <div className="bg-gray-600 flex-1 flex flex-col">
+        <div className="p-4 shadow-md">General</div>
+        <div className="flex-1 p-4">Messages</div>
       </div>
     </div>
   );
