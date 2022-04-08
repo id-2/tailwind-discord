e2d1a17c52f30da4c9baaed1d74a4657c0eb61e9
yushan <yushanwebdev@gmail.com>
2022-04-08 19:50:51 +0530

    feat: Solving the Duplication Problem with Components

---
+++ src/Server.jsx
@@ -0,0 +1,34 @@
+import { useParams } from "react-router-dom";
+
+const Server = () => {
+  const { id } = useParams();
+
+  return (
+    <>
+      <div className="bg-gray-800 w-60 flex flex-col">
+        <div className="px-3 shadow-md h-12 flex items-center text-white font-title">
+          Server {id}
+        </div>
+        <div className="p-3 flex-1 overflow-y-scroll space-y-2 font-medium">
+          {[...Array(40)].map((_, i) => (
+            <p>Channels {i}</p>
+          ))}
+        </div>
+      </div>
+      <div className="bg-gray-600 flex-1 flex flex-col">
+        <div className="p-3 shadow-md text-white">General</div>
+        <div className="flex-1 p-4 overflow-y-scroll space-y-2">
+          {[...Array(40)].map((_, i) => (
+            <p>
+              Messages {i}: Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit. Donec sed maximus neque. Duis ut rhoncus nisi. Vestibulum
+              vitae ex odio. Sed sagittis viverra ligula volutpat suscipit.
+            </p>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Server;
--- src/Server1.jsx
+++
@@ -1,3 +0,0 @@
-const Server1 = () => <div>Server 1</div>;
-
-export default Server1;
