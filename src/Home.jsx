0207269f3a2291039b5a795b0ea130f8080d955f
yushan <yushanwebdev@gmail.com>
2022-04-09 20:55:46 +0530

    feat: Building Collapsible Categories with Rotating Arrow Icons

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,42 +1,68 @@
 import { Link, useParams } from "react-router-dom";
+import { useState } from "react";
 import * as Icons from "./components/icons";
 import data from "./data.json";
 
-const Home = () => (
-  <>
-    <div className="flex w-60 flex-col bg-gray-800">
-      <button className="flex h-12 items-center px-4 font-title text-[15px] text-white shadow-sm transition hover:bg-gray-550/[0.16]">
-        <div className="relative mr-1 h-4 w-4">
-          <Icons.Verified className="absolute h-4 w-4 text-gray-550" />
-          <Icons.Check className="absolute h-4 w-4" />
-        </div>
-        Tailwind CSS
-        <Icons.Chevron className="ml-auto h-[18px] w-[18px] opacity-80" />
-      </button>
-      <div className="flex-1 space-y-[21px] overflow-y-scroll pt-3 font-medium">
-        {data[1].categories.map((category) => (
-          <div key={category.id}>
-            {category.label ? (
-              <button className="flex w-full items-center px-0.5 font-title text-xs uppercase tracking-wide text-gray-500 hover:text-gray-100">
-                <Icons.ArrowComponent className="mr-0.5 h-3 w-3" />
-                {category.label}
-              </button>
-            ) : null}
-            <div className="mt-1 space-y-0.5">
-              {category.channels.map((channel) => (
-                <ChannelLink channel={channel} key={channel.id} />
-              ))}
-            </div>
+const Home = () => {
+  const [closedCategories, setClosedCategories] = useState([]);
+
+  const toggleCategory = (categoryID) => {
+    setClosedCategories((prev) =>
+      prev.includes(categoryID)
+        ? prev.filter((item) => item !== categoryID)
+        : [...prev, categoryID]
+    );
+  };
+
+  return (
+    <>
+      <div className="flex w-60 flex-col bg-gray-800">
+        <button className="flex h-12 items-center px-4 font-title text-[15px] text-white shadow-sm transition hover:bg-gray-550/[0.16]">
+          <div className="relative mr-1 h-4 w-4">
+            <Icons.Verified className="absolute h-4 w-4 text-gray-550" />
+            <Icons.Check className="absolute h-4 w-4" />
           </div>
-        ))}
+          Tailwind CSS
+          <Icons.Chevron className="ml-auto h-[18px] w-[18px] opacity-80" />
+        </button>
+        <div className="flex-1 space-y-[21px] overflow-y-scroll pt-3 font-medium">
+          {data[1].categories.map((category) => (
+            <div key={category.id}>
+              {category.label ? (
+                <button
+                  className="flex w-full items-center px-0.5 font-title text-xs uppercase tracking-wide text-gray-500 hover:text-gray-100"
+                  onClick={() => toggleCategory(category.id)}
+                >
+                  <Icons.ArrowComponent
+                    className={`${
+                      closedCategories.includes(category.id) ? "-rotate-90" : ""
+                    } mr-0.5 h-3 w-3 transition`}
+                  />
+                  {category.label}
+                </button>
+              ) : null}
+              <div className="mt-1 space-y-0.5">
+                {category.channels
+                  .filter((channel) =>
+                    closedCategories.includes(category.id)
+                      ? channel.unread
+                      : true
+                  )
+                  .map((channel) => (
+                    <ChannelLink channel={channel} key={channel.id} />
+                  ))}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-    <div className="flex flex-1 flex-col bg-gray-600">
-      <div className="p-3 text-white shadow-sm">General</div>
-      <div className="flex-1 space-y-2 overflow-y-scroll p-4"></div>
-    </div>
-  </>
-);
+      <div className="flex flex-1 flex-col bg-gray-600">
+        <div className="p-3 text-white shadow-sm">General</div>
+        <div className="flex-1 space-y-2 overflow-y-scroll p-4"></div>
+      </div>
+    </>
+  );
+};
 
 function ChannelLink({ channel }) {
   const params = useParams();
@@ -68,7 +94,7 @@ function ChannelLink({ channel }) {
       <Icons.AddPerson
         className={`${
           active ? "opacity-100" : "opacity-0 group-hover:opacity-100"
-        } ml-auto h-4 w-4 text-gray-300  hover:text-gray-200 `}
+        } ml-auto h-4 w-4 text-gray-300 hover:text-gray-200`}
       />
     </Link>
   );
