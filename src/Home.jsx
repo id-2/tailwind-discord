19d6855f1bef5e1d8806e0d1906646926a908e59
yushan <yushanwebdev@gmail.com>
2022-04-09 12:09:21 +0530

    feat: Identifying Repeated Patterns while Dynamically Generating the Channel List

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,42 +1,33 @@
-import {
-  AddPersonIcon,
-  BookIcon,
-  CheckIcon,
-  ChevronIcon,
-  SpeakerphoneIcon,
-  VerifiedIcon,
-} from "./components/icons";
+import * as Icons from "./components/icons";
+import data from "./data.json";
 
 const Home = () => (
   <>
     <div className="flex w-60 flex-col bg-gray-800">
       <button className="flex h-12 items-center px-4 font-title text-[15px] text-white shadow-sm transition hover:bg-gray-550/[0.16]">
         <div className="relative mr-1 h-4 w-4">
-          <VerifiedIcon className="absolute h-4 w-4 text-gray-550" />
-          <CheckIcon className="absolute h-4 w-4" />
+          <Icons.Verified className="absolute h-4 w-4 text-gray-550" />
+          <Icons.Check className="absolute h-4 w-4" />
         </div>
-        Level Up Tutorials
-        <ChevronIcon className="ml-auto h-[18px] w-[18px] opacity-80" />
+        Tailwind CSS
+        <Icons.Chevron className="ml-auto h-[18px] w-[18px] opacity-80" />
       </button>
-      <div className="mt-5 flex-1 overflow-y-scroll font-medium">
-        <div className="space-y-0.5">
-          <a
-            href="#"
-            className="group mx-2 flex items-center rounded py-1 pl-2 pr-2.5 text-gray-500 hover:bg-gray-550/40 hover:text-gray-200"
-          >
-            <BookIcon className="mr-1.5 h-5 w-5 text-gray-525" />
-            welcome
-            <AddPersonIcon className="ml-auto h-4 w-4 text-gray-300 opacity-0 hover:text-gray-200 group-hover:opacity-100" />
-          </a>
-          <a
-            href="#"
-            className="group mx-2 flex items-center rounded py-1 pl-2 pr-2.5 text-gray-500 hover:bg-gray-550/40 hover:text-gray-200"
-          >
-            <SpeakerphoneIcon className="mr-1.5 h-5 w-5 text-gray-525" />
-            announcements
-            <AddPersonIcon className="ml-auto h-4 w-4 text-gray-300 opacity-0 hover:text-gray-200 group-hover:opacity-100" />
-          </a>
-        </div>
+      <div className="flex-1 space-y-[21px] overflow-y-scroll pt-3 font-medium">
+        {data[1].categories.map((category) => (
+          <div key={category.id}>
+            {category.label ? (
+              <button className="flex items-center px-0.5 font-title text-xs uppercase tracking-wide text-gray-500">
+                <Icons.ArrowComponent className="mr-0.5 h-3 w-3" />
+                {category.label}
+              </button>
+            ) : null}
+            <div className="mt-1 space-y-0.5">
+              {category.channels.map((channel) => (
+                <ChannelLink channel={channel} />
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
     <div className="flex flex-1 flex-col bg-gray-600">
@@ -45,5 +36,21 @@ const Home = () => (
     </div>
   </>
 );
+
+function ChannelLink({ channel }) {
+  let Icon = channel.icon ? Icons[channel.icon] : Icons.Hashtag;
+
+  return (
+    <a
+      href="#"
+      className="group mx-2 flex items-center rounded py-1 pl-2 pr-2.5 text-gray-500 hover:bg-gray-550/40 hover:text-gray-200"
+      key={channel.id}
+    >
+      <Icon className="mr-1.5 h-5 w-5 text-gray-525" />
+      {channel.label}
+      <Icons.AddPerson className="ml-auto h-4 w-4 text-gray-300 opacity-0 hover:text-gray-200 group-hover:opacity-100" />
+    </a>
+  );
+}
 
 export default Home;
